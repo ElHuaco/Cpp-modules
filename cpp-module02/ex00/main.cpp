263a14135673c4e4f3369c76082484c8a332d945
ElHuaco <alexhuac@gmail.com>
2021-02-25 09:51:49 +0100

    Start M02

---
+++ cpp-module02/ex00/main.cpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aleon-ca <aleon-ca@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/02/25 09:49:42 by aleon-ca          #+#    #+#             */
+/*   Updated: 2021/02/25 09:51:00 by aleon-ca         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include "Fixed.class.hpp"
+
+int main( void )
+{
+	Fixed a;
+	Fixed b( a );
+	Fixed c;
+	c = b;
+	std::cout << a.getRawBits() << std::endl;
+	std::cout << b.getRawBits() << std::endl;
+	std::cout << c.getRawBits() << std::endl;
+	return 0;
+}
