420fdfc217efa4eec23929cfc2f52bfff8ce04d5
ElHuaco <alexhuac@gmail.com>
2021-03-11 12:34:09 +0100

    M05-ex03 done

--- cpp-module05/ex03/main.cpp
+++ cpp-module05/ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: alejandroleon <aleon-ca@student.42.fr      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/10 11:30:24 by alejandro         #+#    #+#             */
-/*   Updated: 2021/03/11 11:58:10 by alejandro        ###   ########.fr       */
+/*   Updated: 2021/03/11 12:33:37 by alejandro        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 #include "ShrubberyCreationForm.hpp"
 #include "RobotomyRequestForm.hpp"
 #include "PresidentialPardonForm.hpp"
+#include "Intern.hpp"
 
 int		main(void)
 {
@@ -27,25 +28,47 @@ int		main(void)
 	A2.promote();
 	std::cout << A2 << std::endl;
 	std::cout << std::endl;	
+	std::cout << "======Exceptions on intern form creation=======" << std::endl;
+	Intern	someRandomIntern;
+	Form	*S1 = someRandomIntern.makeForm("shrubbery creation", "Marte");
+	Form	*R1 = someRandomIntern.makeForm("robotomy request", "Bender");
+	Form	*P1 = someRandomIntern.makeForm("presidential pardon", "Buen Ladrón");
+	try
+	{
+		Form	*E1 = someRandomIntern.makeForm("laoalaola", "laoalao"); 
+	}
+	catch (Intern::InternDidNotFindException & e)
+	{
+		std::cout << e.what() << std::endl;
+	}
+	try
+	{
+		Form	*E2 = someRandomIntern.makeForm("", "laoalao"); 
+	}
+	catch (Intern::InternDidNotFindException & e)
+	{
+		std::cout << e.what() << std::endl;
+	}
+	std::cout << std::endl;	
 	std::cout << "==========Exceptions on executions=============" << std::endl;
-	ShrubberyCreationForm	S1("Marte");
-	A2.executeForm(S1);
-	A2.signForm(S1);
-	A1.executeForm(S1);
-	A2.executeForm(S1);
+	A2.executeForm(*S1);
+	A2.signForm(*S1);
+	A1.executeForm(*S1);
+	A2.executeForm(*S1);
 	std::cout << std::endl;	
-	RobotomyRequestForm		R1("Bender");
-	A2.executeForm(R1);
-	A2.signForm(R1);
-	A1.executeForm(R1);
-	A2.executeForm(R1);
-	A2.executeForm(R1);
+	A2.executeForm(*R1);
+	A2.signForm(*R1);
+	A1.executeForm(*R1);
+	A2.executeForm(*R1);
+	A2.executeForm(*R1);
 	std::cout << std::endl;	
-	PresidentialPardonForm	P1("Buen Ladrón");
-	A2.executeForm(P1);
-	A2.signForm(P1);
-	A1.executeForm(P1);
-	A2.executeForm(P1);
+	A2.executeForm(*P1);
+	A2.signForm(*P1);
+	A1.executeForm(*P1);
+	A2.executeForm(*P1);
 	std::cout << std::endl;	
+	delete S1;
+	delete R1;
+	delete P1;
 	return (0);
 }
