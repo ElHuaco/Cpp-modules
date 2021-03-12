3ec3c786c08e9d767bfcba3671b77e2026fbe252
ElHuaco <alexhuac@gmail.com>
2021-03-12 20:15:53 +0100

    M06-ex00 done

--- cpp-module06/ex00/convert.cpp
+++ cpp-module06/ex00/convert.cpp
@@ -6,14 +6,13 @@
 /*   By: alejandroleon <aleon-ca@student.42.fr      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/12 13:22:16 by alejandro         #+#    #+#             */
-/*   Updated: 2021/03/12 14:07:35 by alejandro        ###   ########.fr       */
+/*   Updated: 2021/03/12 20:14:30 by alejandro        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # include <string>
 # include <iostream>
 # include <sstream>
-# include <cctype>
 
 static int	strtoi(std::string str)
 {
@@ -26,26 +25,62 @@ static int	strtoi(std::string str)
 int		main(int argc, char **argv)
 {
 	if (argc != 2)
+	{
 		std::cout << "Bad argument count. Need one literal value." << std::endl;
+		return (1);
+	}
+	std::cout << "====================================" << std::endl;
 	std::string input(argv[1]);
-	if (!isdigit(argv[1][0]))
 	{
-		if (!isprint(argv[1][0]))
-			std::cout << "char: Non displayable" << std::endl;
+		std::cout << "char: ";
+		if ((input.find_first_not_of("+-0123456789.f") != std::string::npos))
+		{
+			if (input.length() != 1)
+				std::cout << "impossible" << std::endl;
+			else
+				std::cout << input << std::endl;
+		}
 		else
-			std::cout << "char: " << argv[1] << std::endl;
-		std::cout << "int: impossible" << std::endl;
-		std::cout << "float: impossible" << std::endl;
-		std::cout << "double: impossible" << std::endl;
+		{
+			int		temp = strtoi(input);
+			if ((temp < 0) || (temp > 127))
+				std::cout << "impossible";
+			else if (isprint(static_cast<char>(temp)))
+				std::cout << static_cast<char>(temp);
+			else
+				std::cout << "Non displayable";
+			std::cout << std::endl;
+		}
 	}
-	else
 	{
-		if (!isprint(argv[1][0]))
-			std::cout << "char: Non displayable" << std::endl;
+		std::cout << "int: ";
+		if ((input == "nan") || (input == "inf") || (input == "-inf")
+			|| (input == "inff") || (input == "-inff") || (input == "nanf")
+			|| (input.find_first_not_of("+-0123456789.f") != std::string::npos))
+			std::cout << "impossible" << std::endl;
 		else
-			std::cout << "char: " << static_cast<char*>(argv[1]) << std::endl;
-		std::cout << "int: " << strtoi(input) << std::endl;
-		std::cout << "float: " << static_cast<float>(atof(argv[1])) << "f" << std::endl;
-		std::cout << "double: " << static_cast<double>(atof(argv[1])) << std::endl;
+			std::cout << strtoi(input) << std::endl;
 	}
+	{
+		std::cout << "float: ";
+		if ((input == "nan") || (input == "inf") || (input == "-inf")
+			|| (input == "inff") || (input == "-inff") || (input == "nanf"))
+			std::cout << input << std::endl;
+		else if (input.find_first_not_of("+-0123456789.f") != std::string::npos)
+			std::cout << "impossible" << std::endl;
+		else
+			std::cout << static_cast<float>(atof(argv[1])) << "f" << std::endl;
+	}
+	{
+		std::cout << "double: "; 
+		if ((input == "nan") || (input == "inf") || (input == "-inf")
+			|| (input == "inff") || (input == "-inff") || (input == "nanf"))
+			std::cout << input << std::endl;
+		else if (input.find_first_not_of("+-0123456789.f") != std::string::npos)
+			std::cout << "impossible" << std::endl;
+		else
+			std::cout << static_cast<double>(atof(argv[1])) << std::endl;
+	}
+	std::cout << "====================================" << std::endl;
+	return (0);
 }
