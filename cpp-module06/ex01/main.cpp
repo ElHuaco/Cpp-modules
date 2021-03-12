42a490e167c664b481f6f3cf5a74358956f335fb
ElHuaco <alexhuac@gmail.com>
2021-03-12 18:37:15 +0100

    M06-ex01 done

--- cpp-module06/ex01/main.cpp
+++ cpp-module06/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: alejandroleon <aleon-ca@student.42.fr      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/12 14:47:19 by alejandro         #+#    #+#             */
-/*   Updated: 2021/03/12 17:23:15 by alejandro        ###   ########.fr       */
+/*   Updated: 2021/03/12 18:36:26 by alejandro        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,14 +36,14 @@ static char		random_alphanum(void)
 
 static void		*serialize(void)
 {
-	void	*result = ::operator new (20);
+	char	*result = new char[20];
 	int		temp = rand() % 100;
 	int		i = -1;
 
 	std::cout << "=====Called serialize.=====" << std::endl;
 	while (++i < 8)
 		reinterpret_cast<char *>(result)[i] = random_alphanum();
-	reinterpret_cast<int *>(result)[8] = temp;
+	*reinterpret_cast<int *>(result + 8) = temp;
 	i = 11;
 	while (++i < 20)
 		reinterpret_cast<char *>(result)[i] = random_alphanum();
@@ -53,13 +53,12 @@ static void		*serialize(void)
 		std::cout << reinterpret_cast<char *>(result)[i];
 	std::cout << std::endl;
 	std::cout << "Raw[8-11]: ";
-	std::cout << reinterpret_cast<int *>(result)[8] << std::endl;
+	std::cout << *reinterpret_cast<int *>(result + 8) << std::endl;
 	i = 11;
 	std::cout << "Raw[12-20]: ";
 	while (++i < 20)
 		std::cout << reinterpret_cast<char *>(result)[i];
 	std::cout << std::endl;
-
 	std::cout << std::endl;
 	return (result);
 }
@@ -69,16 +68,18 @@ static Data		*deserialize(void *raw)
 	std::cout << "====Called deserialize.====" << std::endl;
 	Data		*result = new Data;
 	int			i;
+	char		*temp;
 
+	temp = reinterpret_cast<char *>(raw);
 	result->s1.resize(8);
 	result->s2.resize(8);
 	i = -1;
 	while (++i < 8)
-		result->s1.at(i) = reinterpret_cast<char *>(temp)[i];
-	result->n = reinterpret_cast<int *>(temp)[8];
+		result->s1.at(i) = reinterpret_cast<char *>(raw)[i];
+	result->n = *reinterpret_cast<int *>(temp + 8);
 	i = 11;
 	while (++i < 20)
-		result->s2.at(i - 12) = reinterpret_cast<char *>(temp)[i];
+		result->s2.at(i - 12) = reinterpret_cast<char *>(raw)[i];
 	return (result);
 }
 
@@ -97,7 +98,7 @@ int		main(void)
 	std::cout << "Data s2: " << test->s2 << std::endl;
 	std::cout << std::endl;
 
-	delete reinterpret_cast<char *>(raw);
+	delete [] reinterpret_cast<char *>(raw);
 	delete test;
 	return(0);
 }
