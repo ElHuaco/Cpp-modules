e757840629bbfa71c856d01c33ba86a06c22cf4f
ElHuaco <alexhuac@gmail.com>
2021-03-16 12:38:54 +0100

    M07-ex02 detalles

--- cpp-module07/ex02/main.cpp
+++ cpp-module07/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: aleon-ca <aleon-ca@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/16 10:50:06 by aleon-ca          #+#    #+#             */
-/*   Updated: 2021/03/16 12:32:45 by aleon-ca         ###   ########.fr       */
+/*   Updated: 2021/03/16 12:38:07 by aleon-ca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,8 @@
 
 namespace my
 {
-	template <typename T> void iter(T arr[], int length, void (foo)(T const &))
+	template <typename T>
+		void iter(T arr[], int length, void (foo)(T const &))
 	{
 		int		i;
 
@@ -70,9 +71,9 @@ int		main(void)
 	std::cout << "-------------Copia cambiada-----------" << std::endl;
 	Array<int> I1(A1);
 	I1[0] = 1234;
-	my::iter(I1, 5, print);
+	my::iter(I1, I1.size(), print);
 	std::cout << "---------------Antigua---------------" << std::endl;
-	my::iter(A1, 5, print);
+	my::iter(A1, A1.size(), print);
 	std::cout << "============STR TEST===============" << std::endl;
 	std::string *s = new std::string[5];
 	my::iter(s, 5, print);
@@ -88,7 +89,7 @@ int		main(void)
 	Awesome *a = new Awesome[5];
 	my::iter(a, 5, print);
 	Array<Awesome> A2(5);
-	my::iter(A2, 5, print);
+	my::iter(A2, A2.size(), print);
 	std::cout << "-------------Copia cambiada-----------" << std::endl;
 	Array<Awesome> I3(A2);
 	I3[0].set(69);
