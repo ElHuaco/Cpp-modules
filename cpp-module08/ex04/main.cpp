8c3919a61505aba9f7ac8b07c64d93d4b55c935b
ElHuaco <alexhuac@gmail.com>
2021-03-20 12:41:25 +0100

    M08-ex04 working

--- cpp-module08/ex04/main.cpp
+++ cpp-module08/ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: alejandroleon <aleon-ca@student.42.fr      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/20 10:58:12 by alejandro         #+#    #+#             */
-/*   Updated: 2021/03/20 11:55:17 by alejandro        ###   ########.fr       */
+/*   Updated: 2021/03/20 12:38:13 by alejandro        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,7 +51,7 @@ int		main(int argc, char **argv)
 	std::string input(argv[1]);
 	if (input.find_first_not_of("0123456789 -+*/()") != std::string::npos)
 	{
-		std::cerr << "ex04: error. Infix expression must contain only digits, spaces, '-', '+', '*' and '/'" << std::endl;
+		std::cerr << "ex04: error. Infix expression must contain only digits, spaces, '-', '+', '*', '/' and parenthesis." << std::endl;
 		return (EXIT_FAILURE);
 	}
 	std::deque<Token *>	InfixTokens;
@@ -70,6 +70,46 @@ int		main(int argc, char **argv)
 	}
 	std::cout << "Tokens: ";
 	for_each(InfixTokens.begin(), InfixTokens.end(), my::print);
+	std::cout << std::endl;
+	std::stack<Token *> TempStack;
+	std::deque<Token *> PostfixTokens;
+	std::deque<Token *>::const_iterator i = InfixTokens.begin();
+	while (i != InfixTokens.end())
+	{
+		if ((*i)->getType() == "Operand")
+			PostfixTokens.push_back(*i);
+		else if ((*i)->getType() == "Operator")
+		{
+			while ((TempStack.empty() == false)
+				&& (TempStack.top()->getType() != "ParOpen")
+				&& (dynamic_cast<Operator *>(TempStack.top())->getPrecedence() >=
+					dynamic_cast<Operator *>(*i)->getPrecedence()))
+			{
+				PostfixTokens.push_back(TempStack.top());
+				TempStack.pop();
+			}
+			TempStack.push(*i);
+		}
+		else if ((*i)->getType() == "ParOpen")
+			TempStack.push(*i);
+		else if ((*i)->getType() == "ParClose")
+		{
+			while (TempStack.top()->getType() != "ParOpen")
+			{
+				PostfixTokens.push_back(TempStack.top());
+				TempStack.pop();
+			}
+			TempStack.pop();
+		}
+		++i;
+	}
+	while (TempStack.empty() == false)
+	{
+		PostfixTokens.push_back(TempStack.top());
+		TempStack.pop();
+	}
+	std::cout << "Postfix: ";
+	for_each(PostfixTokens.begin(), PostfixTokens.end(), my::print);
 	std::cout << std::endl;
 	return(EXIT_SUCCESS);
 }
